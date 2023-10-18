daf29b3a3aa1ee2880ff68e7d105a7a94e1b9ff8
Daniel Alencar <danielmalencar2011@gmail.com>
2023-10-18 02:09:05 -0300

    feat: add auth handler ao componente header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,9 @@
+"use client";
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentIcon,
   ShoppingCartIcon,
@@ -9,8 +11,21 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -26,10 +41,53 @@ const Header = () => {
           </SheetHeader>
 
           <div className="mt-2 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <LogInIcon />
-              Fazer Login
-            </Button>
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2 py-4">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data.user.name?.[0].toUpperCase()}
+                    </AvatarFallback>
+
+                    {data?.user?.image && (
+                      <AvatarImage
+                        src={data.user.image}
+                        alt={data?.user?.name || ""}
+                      />
+                    )}
+                  </Avatar>
+
+                  <div className="flex flex-col">
+                    <p className="font-thin">{data.user.name}</p>
+                    <p className="text-sm opacity-75">Boas compras!</p>
+                  </div>
+                </div>
+                <Separator />
+              </div>
+            )}
+            <div className="mt-4 flex flex-col gap-2">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon />
+                  Fazer Login
+                </Button>
+              )}
+
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon />
+                  Fazer Logout
+                </Button>
+              )}
+            </div>
 
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
@@ -49,7 +107,7 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-      <h1 className="text-lg font-semibold">
+      <h1 className="text-2xl font-semibold">
         <span className="text-primary">NextJS</span> Store
       </h1>
 
