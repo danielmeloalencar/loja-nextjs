5472ef49d10a04ec83932b1e7c0ebed37c56770c
Daniel Alencar <danielmalencar2011@gmail.com>
2023-10-21 03:14:06 -0300

    refact: code format

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -1,16 +1,16 @@
 import Image, { ImageProps } from "next/image";
 
-const PromoBanner = ({alt,...props}:ImageProps) => {
-  return (  
+const PromoBanner = ({ alt, ...props }: ImageProps) => {
+  return (
     <Image
-        height={0}
-        width={0}
-        className="h-auto w-full px-5"
-        sizes="100vw"
-        alt={alt}
-        {...props}
-      />
+      height={0}
+      width={0}
+      className="h-auto w-full px-5"
+      sizes="100vw"
+      alt={alt}
+      {...props}
+    />
   );
-}
- 
+};
+
 export default PromoBanner;
